7337a13bca77875cb5e05bcfbe53e82b3b61cdb5
darkheart101 <kurokokoro@windowslive.com>
2020-02-09 08:37:36 +0200

    feature: create new tweet

--- src/main/java/com/tkouleris/tweety/controller/TweetController.java
+++ src/main/java/com/tkouleris/tweety/controller/TweetController.java
@@ -2,7 +2,6 @@ package com.tkouleris.tweety.controller;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.tkouleris.tweety.dao.UserRepository;
 import com.tkouleris.tweety.model.Tweet;
@@ -26,7 +27,7 @@ public class TweetController
 	@Autowired
 	private TweetService tweetService;
 	
-	@GetMapping(path = "/feed", produces = "application/json")
+	@GetMapping(path = "/tweet/feed", produces = "application/json")
 	public ResponseEntity<Object> getFeed(Authentication authentication)
 	{			
         User LoggedInUser = R_User.findByUsername(authentication.getName());
@@ -39,4 +40,20 @@ public class TweetController
 	    
 		return new ResponseEntity<>(body,HttpStatus.OK);
 	}
+	
+	@PostMapping(path = "/tweet/create", produces = "application/json")
+	public ResponseEntity<Object> createTweet(Authentication authentication, @RequestBody Tweet newTweet)
+	{			
+		Tweet savedTweet = tweetService.createTweet(authentication,newTweet);
+		/*
+        User LoggedInUser = R_User.findByUsername(authentication.getName());
+        Tweet latestUserTweet = tweetService.getFeed(LoggedInUser);
+        	    */
+	    Map<String, Object> body = new LinkedHashMap<>();
+	    body.put("timestamp", LocalDateTime.now());
+	    body.put("message", "Tweet created!");
+	    body.put("data", savedTweet );
+
+		return new ResponseEntity<>(body,HttpStatus.OK);
+	}	
 }
--- src/main/java/com/tkouleris/tweety/service/TweetService.java
+++ src/main/java/com/tkouleris/tweety/service/TweetService.java
@@ -1,11 +1,14 @@
 package com.tkouleris.tweety.service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.tkouleris.tweety.dao.TweetRepository;
+import com.tkouleris.tweety.dao.UserRepository;
 import com.tkouleris.tweety.model.Tweet;
 import com.tkouleris.tweety.model.User;
 
@@ -14,10 +17,24 @@ public class TweetService {
 	
 	@Autowired
 	TweetRepository R_Tweet;
+	@Autowired
+	UserRepository R_User;
+//	@Autowired
+//	Authentication authentication;
 	
 	public Tweet getFeed(User loggedInUser)
 	{
 		List<Tweet> userTweets = R_Tweet.findLatestTweetByUser(loggedInUser.getUser_id());
 		return userTweets.get(0);
+	}
+	
+	public Tweet createTweet(Authentication authentication, Tweet newTweet)
+	{		
+        User LoggedInUser = R_User.findByUsername(authentication.getName());
+        newTweet.setTweet_user_id(LoggedInUser);
+        Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+        newTweet.setTweet_created_at(currentTimestamp );
+        newTweet.setTweet_updated_at(currentTimestamp );
+        return R_Tweet.save(newTweet);
 	}
 }
