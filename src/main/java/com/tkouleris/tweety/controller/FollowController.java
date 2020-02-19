d00922d86b1df22e17fcd61ad86de1514f60a237
darkheart101 <kurokokoro@windowslive.com>
2020-02-19 06:34:17 +0200

    refactor: follow service

--- src/main/java/com/tkouleris/tweety/controller/FollowController.java
+++ src/main/java/com/tkouleris/tweety/controller/FollowController.java
@@ -13,36 +13,23 @@ import com.tkouleris.tweety.dao.UserRepository;
 import com.tkouleris.tweety.model.Follower;
 import com.tkouleris.tweety.model.User;
 import com.tkouleris.tweety.responses.ApiResponse;
+import com.tkouleris.tweety.service.FollowService;
 
 @Controller
 public class FollowController {
-	@Autowired
-	private UserRepository R_User;
+	
 	@Autowired
 	private ApiResponse apiResponse;
 	@Autowired
-	private FollowerRepository R_follower;
+	FollowService followService;
 	
-	@PostMapping(path = "follow/add/{user_id}", produces = "application/json")
+	@PostMapping(path = "follow/user/{user_id}", produces = "application/json")
 	public ResponseEntity<Object> addFolower(
 			Authentication authentication, 
 			@PathVariable("user_id") long user_id
 	) throws Exception
 	{			    		
-		User LoggedInUser = R_User.findByUsername(authentication.getName());
-		User FolloweeUser = R_User.findById(user_id).orElse(null);
-		
-		if(FolloweeUser == null)
-		{
-			throw new Exception("User not found!");
-		}
-		
-		Follower f = new Follower();
-		f.setFollower(LoggedInUser);
-		f.setFollowee(FolloweeUser);
-		
-		R_follower.save(f);
-		
+		followService.followUser(authentication, user_id);
 	    apiResponse.setMessage("Followee Added!");
 	    apiResponse.setData(user_id);
 
---
+++ src/main/java/com/tkouleris/tweety/service/FollowService.java
@@ -0,0 +1,33 @@
+package com.tkouleris.tweety.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import com.tkouleris.tweety.dao.FollowerRepository;
+import com.tkouleris.tweety.dao.UserRepository;
+import com.tkouleris.tweety.model.Follower;
+import com.tkouleris.tweety.model.User;
+
+@Service
+public class FollowService {
+	
+	@Autowired
+	private UserRepository R_User;
+	@Autowired
+	FollowerRepository R_Follower;
+	
+	public void followUser(Authentication authentication, Long user_id) throws Exception
+	{
+		User LoggedInUser = R_User.findByUsername(authentication.getName());
+		User FolloweeUser = R_User.findById(user_id).orElse(null);		
+		if(FolloweeUser == null)
+		{
+			throw new Exception("User not found!");
+		}		
+		Follower f = new Follower();
+		f.setFollower(LoggedInUser);
+		f.setFollowee(FolloweeUser);		
+		R_Follower.save(f);
+	}
+}
