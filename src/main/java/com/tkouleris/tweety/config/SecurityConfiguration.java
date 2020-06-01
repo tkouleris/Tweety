61108c1b8b25b8070d88f29b2fd80227bd8bcd9c
darkheart101 <kurokokoro@windowslive.com>
2020-06-01 07:20:05 +0300

    minor fix

--- src/main/java/com/tkouleris/tweety/config/SecurityConfiguration.java
+++ src/main/java/com/tkouleris/tweety/config/SecurityConfiguration.java
@@ -2,9 +2,11 @@ package com.tkouleris.tweety.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -52,6 +54,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 	    return super.authenticationManagerBean();
 	}
+	
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
